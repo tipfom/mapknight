3547ee41fd629a8d7fcbddad43224126c542d541
Tim <tipfom@hotmail.de>
2016-04-24 16:08:32 +0200

    changed namespace

---
+++ _Android/CGL/Entity/CGLEntitySet.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+using Android.Content;
+
+using mapKnight.Basic;
+
+namespace mapKnight.Android.CGL.Entity {
+    public class CGLSet : Set {
+        public readonly CGLTexture2D Texture;
+
+        public CGLSet (XMLElemental setConfig, Context context) : base (setConfig) {
+            // load texture
+            Texture = Assets.LoadTexture (Path.Combine ("sets", Name + ".png"));
+        }
+    }
+}
