24707b1210523e4909aa3eadcf7aef58a0ec335f
tipfom <tipfom@hotmail.de>
2016-05-14 20:27:43 +0200

    updated versionnumber
    the entity component system is now completed.
    minor changes and component additions might be done later.

--- Android/Properties/AssemblyInfo.cs
+++ Android/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-﻿using System.Reflection;
-using Android.App;
+﻿using Android.App;
+using System.Reflection;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
@@ -17,7 +17,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("2.1.74.*")]
+[assembly: AssemblyVersion ("2.1.85.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
