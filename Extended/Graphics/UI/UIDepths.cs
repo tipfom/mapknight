6ab2e1f33039b2f5730e1650a40bbcaba66ff560
Tim <tipfom@hotmail.de>
2016-12-18 15:42:39 +0100

    added ui depths enumeration

---
+++ Extended/Graphics/UI/UIDepths.cs
@@ -0,0 +1,9 @@
+﻿namespace mapKnight.Extended.Graphics.UI
+{
+    public static class UIDepths
+    {
+        public const int FOREGROUND = 2;
+        public const int MIDDLE = 1;
+        public const int BACKGROUND = 0;
+    }
+}
