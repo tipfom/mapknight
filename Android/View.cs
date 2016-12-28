f05ff58acced63e924bffac212f4d398dbf4b98f
Tim <tipfom@hotmail.de>
2016-12-28 20:15:16 +0100

    changed the android view to use the opentk native features

--- Android/View.cs
+++ Android/View.cs
@@ -1,7 +1,35 @@
-﻿using Android.Content;
+﻿using System;
+using Android.Content;
 using Android.Opengl;
+using OpenTK;
+using OpenTK.Platform.Android;
+using mapKnight.Extended;
+using mapKnight.Extended.Graphics;
 
 namespace mapKnight.Android {
+    public class View : AndroidGameView {
+        public View (Context context) : base(context) {
+            ContextRenderingApi = OpenTK.Graphics.GLVersion.ES2;
+            
+        }
+
+        protected override void OnSizeChanged (int w, int h, int oldw, int oldh) {
+            Window.Change(w, h);
+            base.OnSizeChanged(w, h, oldw, oldh);
+        }
+
+        protected override void OnLoad (EventArgs e) {
+            base.OnLoad(e);
+            Manager.Initialize( );
+            Run( );
+        }
+
+        protected override void OnRenderFrame (FrameEventArgs e) {
+            Manager.Update( );
+            SwapBuffers( );
+        }
+    }
+    /*
     public class View : GLSurfaceView {
         public View (Context context) : base(context) {
             base.SetEGLConfigChooser(8, 8, 8, 8, 16, 0);
@@ -11,5 +39,5 @@ namespace mapKnight.Android {
             this.RenderMode = Rendermode.Continuously;
         }
     }
-
+    */
 }
