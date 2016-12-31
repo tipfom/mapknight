64a673f85f18d567315ef306cbe9e398cd999d82
Tim <tipfom@hotmail.de>
2016-12-31 14:44:19 +0100

    fixed bug causing the slime to walk through blocks

--- Extended/Components/AI/Basics/WallWalkerComponent.cs
+++ Extended/Components/AI/Basics/WallWalkerComponent.cs
@@ -77,15 +77,15 @@ namespace mapKnight.Extended.Components.AI.Basics {
                     int ywalkingon = CurrentWallDir == Direction.Down ? Mathi.Floor(Owner.Transform.BL.Y) - 1 : Mathi.Floor(Owner.Transform.TR.Y);
                     int ywalkingagainst = CurrentWallDir == Direction.Down ? ywalkingon + 1 : ywalkingon - 1;
                     for (int x = Mathi.Floor(Owner.Transform.BL.X) - 1; x > -1; x--) {
-                        if (!Owner.World.HasCollider(x, ywalkingon)) {
-                            targetLoc = x + 1 - Owner.Transform.HalfSize.X;
-                            NextMoveDir = CurrentWallDir;
-                            NextWallDir = Direction.Right;
-                            break;
-                        } else if (Owner.World.HasCollider(x, ywalkingagainst)) {
+                        if (Owner.World.HasCollider(x, ywalkingagainst)) {
                             targetLoc = x + 1 + Owner.Transform.HalfSize.X;
                             NextMoveDir = 1 - CurrentWallDir;
                             NextWallDir = Direction.Left;
+                            break;
+                        } else if (!Owner.World.HasCollider(x, ywalkingon)) {
+                            targetLoc = x + 1 - Owner.Transform.HalfSize.X;
+                            NextMoveDir = CurrentWallDir;
+                            NextWallDir = Direction.Right;
                             break;
                         }
                     }
@@ -96,15 +96,15 @@ namespace mapKnight.Extended.Components.AI.Basics {
                     ywalkingon = CurrentWallDir == Direction.Down ? Mathi.Floor(Owner.Transform.BL.Y) - 1 : Mathi.Floor(Owner.Transform.TR.Y);
                     ywalkingagainst = CurrentWallDir == Direction.Down ? ywalkingon + 1 : ywalkingon - 1;
                     for (int x = Mathi.Floor(Owner.Transform.TR.X); x < Owner.World.Size.Width; x++) {
-                        if (!Owner.World.HasCollider(x, ywalkingon)) {
-                            targetLoc = x + Owner.Transform.HalfSize.X;
-                            NextMoveDir = CurrentWallDir;
-                            NextWallDir = Direction.Left;
-                            break;
-                        } else if (Owner.World.HasCollider(x, ywalkingagainst)) {
+                        if (Owner.World.HasCollider(x, ywalkingagainst)) {
                             targetLoc = x - Owner.Transform.HalfSize.X;
                             NextMoveDir = 1 - CurrentWallDir;
                             NextWallDir = Direction.Right;
+                            break;
+                        } else if (!Owner.World.HasCollider(x, ywalkingon)) {
+                            targetLoc = x + Owner.Transform.HalfSize.X;
+                            NextMoveDir = CurrentWallDir;
+                            NextWallDir = Direction.Left;
                             break;
                         }
                     }
@@ -115,15 +115,15 @@ namespace mapKnight.Extended.Components.AI.Basics {
                     int xwalkingon = CurrentWallDir == Direction.Left ? Mathi.Floor(Owner.Transform.BL.X) - 1 : Mathi.Floor(Owner.Transform.TR.X);
                     int xwalkingagainst = CurrentWallDir == Direction.Left ? xwalkingon + 1 : xwalkingon - 1;
                     for (int y = Mathi.Floor(Owner.Transform.TR.Y); y < Owner.World.Size.Height; y++) {
-                        if (!Owner.World.HasCollider(xwalkingon, y)) {
-                            targetLoc = y + Owner.Transform.HalfSize.Y;
-                            NextMoveDir = CurrentWallDir;
-                            NextWallDir = Direction.Down;
-                            break;
-                        } else if (Owner.World.HasCollider(xwalkingagainst, y)) {
+                        if (Owner.World.HasCollider(xwalkingagainst, y)) {
                             targetLoc = y - Owner.Transform.HalfSize.Y;
                             NextMoveDir = 1 - CurrentWallDir;
                             NextWallDir = Direction.Up;
+                            break;
+                        } else if (!Owner.World.HasCollider(xwalkingon, y)) {
+                            targetLoc = y + Owner.Transform.HalfSize.Y;
+                            NextMoveDir = CurrentWallDir;
+                            NextWallDir = Direction.Down;
                             break;
                         }
                     }
@@ -134,15 +134,15 @@ namespace mapKnight.Extended.Components.AI.Basics {
                     xwalkingon = CurrentWallDir == Direction.Left ? Mathi.Floor(Owner.Transform.BL.X) - 1 : Mathi.Floor(Owner.Transform.TR.X);
                     xwalkingagainst = CurrentWallDir == Direction.Left ? xwalkingon + 1 : xwalkingon - 1;
                     for (int y = Mathi.Floor(Owner.Transform.BL.Y) - 1; y > -1; y--) {
-                        if (!Owner.World.HasCollider(xwalkingon, y)) {
-                            targetLoc = y + 1 - Owner.Transform.HalfSize.Y;
-                            NextMoveDir = CurrentWallDir;
-                            NextWallDir = Direction.Up;
-                            break;
-                        } else if (Owner.World.HasCollider(xwalkingagainst, y)) {
+                        if (Owner.World.HasCollider(xwalkingagainst, y)) {
                             targetLoc = y + 1 + Owner.Transform.HalfSize.Y;
                             NextMoveDir = 1 - CurrentWallDir;
                             NextWallDir = Direction.Down;
+                            break;
+                        } else if (!Owner.World.HasCollider(xwalkingon, y)) {
+                            targetLoc = y + 1 - Owner.Transform.HalfSize.Y;
+                            NextMoveDir = CurrentWallDir;
+                            NextWallDir = Direction.Up;
                             break;
                         }
                     }
