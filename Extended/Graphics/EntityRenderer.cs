94e6445c9c62f710b9cc675d3b73d196c88fbfd1
Tim <tipfom@hotmail.de>
2017-01-19 19:16:50 +0100

    entities are now batched together based on their texture and not based on their species

--- Extended/Graphics/EntityRenderer.cs
+++ Extended/Graphics/EntityRenderer.cs
@@ -9,21 +9,22 @@ namespace mapKnight.Extended.Graphics {
         const int MAX_QUAD_COUNT = 400;
 
         private Dictionary<int, SpriteBatch> entityTextures = new Dictionary<int, SpriteBatch>( );
-        private Dictionary<int, Queue<VertexData>> frameVertexData = new Dictionary<int, Queue<VertexData>>( );
+        private Dictionary<SpriteBatch, Queue<VertexData>> frameVertexData = new Dictionary<SpriteBatch, Queue<VertexData>>( );
 
         private BufferBatch buffer;
         private ClientBuffer vertexBuffer { get { return (ClientBuffer)buffer.VertexBuffer; } }
         private ClientBuffer textureBuffer { get { return (ClientBuffer)buffer.TextureBuffer; } }
         private ClientBuffer colorBuffer { get { return (ClientBuffer)buffer.ColorBuffer; } }
 
-        public EntityRenderer ( ) {
+        public EntityRenderer( ) {
             buffer = new BufferBatch(new IndexBuffer(MAX_QUAD_COUNT), new ClientBuffer(2, MAX_QUAD_COUNT, PrimitiveType.Quad), new ClientBuffer(4, MAX_QUAD_COUNT, PrimitiveType.Quad), new ClientBuffer(2, MAX_QUAD_COUNT, PrimitiveType.Quad));
         }
 
-        public void AddTexture (int species, SpriteBatch entityTexture) {
+        public void AddTexture(int species, SpriteBatch entityTexture) {
             if (!entityTextures.ContainsKey(species)) {
                 entityTextures.Add(species, entityTexture);
-                frameVertexData.Add(species, new Queue<VertexData>( ));
+                if (!frameVertexData.ContainsKey(entityTexture))
+                    frameVertexData.Add(entityTexture, new Queue<VertexData>( ));
             }
         }
 
@@ -31,22 +32,22 @@ namespace mapKnight.Extended.Graphics {
             return entityTextures[species];
         }
 
-        public void QueueVertexData (int species, List<VertexData> vertexData) {
-            frameVertexData[species].Enqueue(vertexData);
+        public void QueueVertexData(int species, List<VertexData> vertexData) {
+            frameVertexData[entityTextures[species]].Enqueue(vertexData);
         }
 
-        public void Draw ( ) {
-            foreach (int species in frameVertexData.Keys) {
+        public void Draw( ) {
+            foreach(SpriteBatch sprite in frameVertexData.Keys) { 
                 int currentIndex = 0;
-                while (frameVertexData[species].Count > 0) {
-                    VertexData vertexData = frameVertexData[species].Dequeue( );
+                while (frameVertexData[sprite].Count > 0) {
+                    VertexData vertexData = frameVertexData[sprite].Dequeue( );
                     Array.Copy(vertexData.Verticies, 0, vertexBuffer.Data, currentIndex * 8, 8);
                     Array.Copy(vertexData.Color.ToOpenGL( ), 0, colorBuffer.Data, currentIndex * 16, 16);
-                    Array.Copy(entityTextures[species].Get(vertexData.Texture), 0, textureBuffer.Data, currentIndex * 8, 8);
+                    Array.Copy(sprite.Get(vertexData.Texture), 0, textureBuffer.Data, currentIndex * 8, 8);
                     currentIndex++;
                 }
                 Program.Begin( );
-                Program.Draw(buffer, entityTextures[species], Matrix.Default, currentIndex * 6, 0, true);
+                Program.Draw(buffer, sprite, Matrix.Default, currentIndex * 6, 0, true);
                 Program.End( );
             }
         }
