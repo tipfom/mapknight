9a2661a965920336a5bf1692446bfcea7ee7970d
Tim <tipfom@hotmail.de>
2017-02-01 14:08:21 +0100

    added animations to the npc

--- Extended/Components/AI/NPCComponent.cs
+++ Extended/Components/AI/NPCComponent.cs
@@ -2,18 +2,27 @@
 
 namespace mapKnight.Extended.Components.AI {
     public class NPCComponent : Component {
-        public bool Available = true;
+        private bool _Available = true;
+        public bool Available {
+            get {
+                if (!_Available) {
+                    Owner.SetComponentInfo(ComponentData.SpriteAnimation, "deny", true);
+                    Owner.SetComponentInfo(ComponentData.SpriteAnimation, "idle", false);
+                }
+                return _Available;
+            }
+        }
 
         private string[ ] messages;
         private int currentIndex = -1;
-        
-        public NPCComponent (Entity owner, string[] messages) : base(owner) {
+
+        public NPCComponent (Entity owner, string[ ] messages) : base(owner) {
             owner.Domain = EntityDomain.NPC;
             this.messages = messages;
         }
 
         public override void Collision (Entity collidingEntity) {
-            if(collidingEntity.Domain == EntityDomain.Player) {
+            if (collidingEntity.Domain == EntityDomain.Player) {
                 Owner.SetComponentInfo(ComponentData.Color, Color.Fuchsia);
             }
         }
@@ -22,9 +31,12 @@ namespace mapKnight.Extended.Components.AI {
             currentIndex++;
             if (currentIndex == messages.Length) {
                 currentIndex = -1;
-                Available = false;
+                _Available = false;
+                Owner.SetComponentInfo(ComponentData.SpriteAnimation, "idle", true);
                 return null;
             } else {
+                Owner.SetComponentInfo(ComponentData.SpriteAnimation, "talk", true);
+                Owner.SetComponentInfo(ComponentData.SpriteAnimation, "idle", false);
                 return messages[currentIndex];
             }
         }
