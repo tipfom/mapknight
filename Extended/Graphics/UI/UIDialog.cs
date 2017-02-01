9d72e1011665f61ba00ac501a1d07ff59371554d
Tim <tipfom@hotmail.de>
2017-02-01 15:19:14 +0100

    added a frame to the uidialog

--- Extended/Graphics/UI/UIDialog.cs
+++ Extended/Graphics/UI/UIDialog.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using mapKnight.Extended.Components.AI;
+using mapKnight.Core;
 using mapKnight.Extended.Graphics.UI.Layout;
+using System.Collections.Generic;
 
 namespace mapKnight.Extended.Graphics.UI {
-    public class UIDialog {
+    public class UIDialog : UIItem {
         public event Action DialogFinished;
         UILabel currentPopup;
         NPCComponent npc;
 
-        public UIDialog (Screen owner, NPCComponent npc) {
+        public UIDialog (Screen owner, NPCComponent npc) : base(owner, new UIHorizontalCenterMargin(0f), new UIBottomMargin(0.1f), new Vector2(Window.Ratio * 1.5f, 0.7f), UIDepths.MIDDLE){
             this.npc = npc;
-            currentPopup = new UILabel(owner, new UIHorizontalCenterMargin(0), new UIBottomMargin(0.2f), 0.1f, npc.NextMessage( ), UITextAlignment.Center);
+            currentPopup = new UILabel(owner, new UIHorizontalCenterMargin(0), new UITopMargin(1.35f), UIDepths.FOREGROUND, 0.1f, npc.NextMessage( ), UITextAlignment.Center);
 
             void HandleClick ( )
             {
@@ -19,10 +21,21 @@ namespace mapKnight.Extended.Graphics.UI {
                     currentPopup.Text = nextMessage;
                 } else {
                     currentPopup.Dispose( );
+                    Dispose( );
                     DialogFinished?.Invoke( );
                 }
             }
-            currentPopup.Click += HandleClick;
+            Click += HandleClick;
+
+            Window.Changed += ( ) => { Size =new Vector2(1.5f * Window.Ratio, 0.7f); };
+            IsDirty = true;
+        }
+
+        public override List<DepthVertexData> ConstructVertexData ( ) {
+            List<DepthVertexData> vertexData = new List<DepthVertexData>( );
+            vertexData.Add(new DepthVertexData(Bounds.Verticies, "blank", UIDepths.BACKGROUND, Color.Black));
+            vertexData.Add(new DepthVertexData(new UIRectangle(Bounds.Position + new Vector2(0.05f, -0.05f), Bounds.Size - new Vector2(0.1f, 0.1f)).Verticies,"blank", UIDepths.MIDDLE, Color.Blue));
+            return vertexData;
         }
     }
 }
