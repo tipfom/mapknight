23bf6834bb990c871f910c9dbe9da857655918fa
Tim <tipfom@hotmail.de>
2017-02-02 20:23:14 +0100

    added an rectangle ctor

--- Core/Rectangle.cs
+++ Core/Rectangle.cs
@@ -1,11 +1,16 @@
-﻿using System;
-
-namespace mapKnight.Core {
+﻿namespace mapKnight.Core {
     public struct Rectangle {
         public Vector2 Position;
         public Vector2 Size;
         public float Rotation;
         public bool Flipped;
+
+        public Rectangle (float x, float y, float width, float height, float rotation = 0, bool flipped = false) {
+            this.Position = new Vector2(x, y);
+            this.Size = new Vector2(width, height);
+            this.Rotation = rotation;
+            this.Flipped = flipped;
+        }
 
         public void Verticies ( ref float[] result) {
             Mathf.TransformAtOrigin(Size.ToQuad( ), ref result, Position.X, Position.Y, Rotation, Flipped);
