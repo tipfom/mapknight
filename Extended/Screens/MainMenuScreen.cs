1ca594d8a711ab45df72b4e698e3e49cd1ba146b
Tim <tipfom@hotmail.de>
2017-02-13 18:24:34 +0100

    added information to notify about debug builds

--- Extended/Screens/MainMenuScreen.cs
+++ Extended/Screens/MainMenuScreen.cs
@@ -10,7 +10,12 @@ namespace mapKnight.Extended.Screens {
     public class MainMenuScreen : Screen {
         public override void Load ( ) {
             new UILabel(this, new UIHorizontalCenterMargin(0f), new UITopMargin(0.2f), 0.2f, "MAPKNIGHT");
-            new UILabel(this, new UIRightMargin(0.05f), new UIBottomMargin(0.05f), 0.07f, "VERSION: " + Assembly.GetExecutingAssembly( ).GetName( ).Version.ToString(3));
+            new UILabel(this, new UIRightMargin(0.05f), new UIBottomMargin(0.05f), 0.07f, "VERSION: " + Assembly.GetExecutingAssembly( ).GetName( ).Version.ToString(3)
+#if DEBUG
+                + " - DEBUG"
+#endif                
+                );
+
             UIButton button = new UIButton(this, new UIHorizontalCenterMargin(0f), new UIVerticalCenterMargin(-0.2f), new Vector2(1.3f, 0.3f), "PLAY");
             button.Release += ( ) => { Screen.Active = Gameplay; };
             base.Load( );
