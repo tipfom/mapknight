090fb3bc51fe3525bad356e107bf2176f9455eb3
Tim <tipfom@hotmail.de>
2017-02-19 12:11:41 +0100

    changed the style for the API-v11 not to use the splashscreen

--- Android/Resources/Resource.designer.cs
+++ Android/Resources/Resource.designer.cs
@@ -47,9 +47,6 @@ namespace mapKnight.Android
 			// aapt resource value: 0x7f020000
 			public const int icon = 2130837504;
 			
-			// aapt resource value: 0x7f020001
-			public const int splash = 2130837505;
-			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
