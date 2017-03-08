1de6da73d4bdaa54ad17d6806d5c59549d4f8050
Tim <tipfom@hotmail.de>
2017-03-08 20:10:18 +0100

    added a very basic healthbar

--- Extended/Components/Stats/HealthComponent.cs
+++ Extended/Components/Stats/HealthComponent.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using mapKnight.Core;
+using System.Collections.Generic;
+using mapKnight.Extended.Graphics;
+using mapKnight.Extended.Graphics.UI;
 
 namespace mapKnight.Extended.Components.Stats {
 
     public class HealthComponent : Component {
+        private const int HEALTHBAR_ENTITY_ID = -2;
+        private const float HEALTHBAR_WIDTH_PER_VALUE = 0.1f;
+
         public readonly int Initial;
         public float Current;
         public Func<Entity, bool> IsHit; 
@@ -18,6 +24,8 @@ namespace mapKnight.Extended.Components.Stats {
         public override void Prepare ( ) {
             if (Owner.HasComponent<ArmorComponent>())
                 armorComponent = Owner.GetComponent<ArmorComponent>( );
+            if(!Owner.World.Renderer.HasTexture(HEALTHBAR_ENTITY_ID))
+            Owner.World.Renderer.AddTexture(HEALTHBAR_ENTITY_ID, Assets.Load<SpriteBatch>("interface"));
         }
 
         public override void Update (DeltaTime dt) {
@@ -29,6 +37,18 @@ namespace mapKnight.Extended.Components.Stats {
                         Owner.Destroy( );
                 }
             }
+        }
+
+        public override void PostUpdate( ) {
+            Owner.World.Renderer.QueueVertexData(HEALTHBAR_ENTITY_ID, GetHealthbar( ));
+        }
+
+        private List<VertexData> GetHealthbar( ) {
+            List<VertexData> healthbarData = new List<VertexData>(1);
+            float width = Current * HEALTHBAR_WIDTH_PER_VALUE * Owner.World.VertexSize;
+            float height = 0.1f * Owner.World.VertexSize;
+            healthbarData.Add(new VertexData(UIRectangle.GetVerticies(Owner.PositionOnScreen.X - width / 2f, Owner.PositionOnScreen.Y + 2 * height + Owner.Transform.HalfSize.Y * Owner.World.VertexSize, width, height), "blank", Color.Red));
+            return healthbarData;
         }
 
         public new class Configuration : Component.Configuration {
