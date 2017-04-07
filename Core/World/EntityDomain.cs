ab910b0c236a29bf63d2b1634b8a6d9dc4471e53
Tim <tipfom@hotmail.de>
2017-04-07 17:59:14 +0200

    fixed bug causing enemys to be treated as temporary

--- Core/World/EntityDomain.cs
+++ Core/World/EntityDomain.cs
@@ -1,11 +1,11 @@
 ﻿namespace mapKnight.Core.World {
     // (╯°□°）╯︵ ┻━┻
     public enum EntityDomain {
+        Temporary = 0,  // default, will be assigned to everything not conceirning the player directly 
         Enemy,          // everything that attacks the player and can be attack 
         NPC,            // everything to interact with 
         Obstacle,       // everything dealing damage to the player that cant be attacked directly 
         Platform,       // platforms
         Player,         // the player itself 
-        Temporary = 0,  // default, will be assigned to everything not conceirning the player directly 
     }
 }
