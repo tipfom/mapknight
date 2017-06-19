b425ae510849951b3bfce3d13a2e1f9b684d42eb
Tim <tipfom@hotmail.de>
2017-06-19 20:15:06 +0200

    reworked the AddBrushDialog

--- ToolKit/Windows/Dialogs/AddBrushDialog.xaml.cs
+++ ToolKit/Windows/Dialogs/AddBrushDialog.xaml.cs
@@ -22,6 +22,20 @@ namespace mapKnight.ToolKit.Windows.Dialogs {
         public static readonly RoutedUICommand FinishCommand = new RoutedUICommand("Finish", "FinishCommand", typeof(AddBrushDialog));
         public static readonly RoutedUICommand EditPossibilitiesCommand = new RoutedUICommand("Edit Possibilities", "EditPossibilitiesCommand", typeof(AddBrushDialog));
 
+        public static readonly RoutedUICommand NextCornerTLCommand = new RoutedUICommand("Next Corner TL", "NextCornerTLCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextCornerTRCommand = new RoutedUICommand("Next Corner TR", "NextCornerTRCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextCornerBLCommand = new RoutedUICommand("Next Corner BL", "NextCornerBLCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextCornerBRCommand = new RoutedUICommand("Next Corner BR", "NextCornerBRCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextCentreCommand = new RoutedUICommand("Next Centre", "NextCentreCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextILCommand = new RoutedUICommand("Next IL", "NextILCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextITCommand = new RoutedUICommand("Next IT", "NextITCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextIRCommand = new RoutedUICommand("Next IR", "NextIRCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextIBCommand = new RoutedUICommand("Next IB", "NextIBCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextLTLCommand = new RoutedUICommand("Next LTL", "NextLTLCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextLTRCommand = new RoutedUICommand("Next LTR", "NextLTRCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextLBLCommand = new RoutedUICommand("Next LBL", "NextLBLCommand", typeof(AddBrushDialog));
+        public static readonly RoutedUICommand NextLBRCommand = new RoutedUICommand("Next LBR", "NextLBRCommand", typeof(AddBrushDialog));
+
         public Data.TileBrush DialogResultBrush = new Data.TileBrush( );
 
         private EditorMap map;
@@ -43,14 +57,36 @@ namespace mapKnight.ToolKit.Windows.Dialogs {
                 listview_tiles.Items.Add(new ListViewEntry(map.WpfTextures[tile.Name]));
         }
 
-        private struct ListViewEntry {
-            public ListViewEntry (BitmapImage image) {
-                Image = image;
+        private void SetTile (ref TileBrushStrokeCollection brushRef, ref int index, Image image) {
+            if (listview_tiles.SelectedItem != null) {
+                if (brushRef.Count <= index) {
+                    brushRef.Add(new TileBrushStroke(map.Tiles[listview_tiles.SelectedIndex], (float)(((RotateTransform)image.RenderTransform).Angle / 180), 1));
+                } else {
+                    brushRef[index] = new TileBrushStroke(map.Tiles[listview_tiles.SelectedIndex], (float)(((RotateTransform)image.RenderTransform).Angle / 180), 1);
+                }
             }
-
-            public BitmapImage Image { get; private set; }
         }
 
+        private void ImageClick (Image image, BitmapImage defaultImage, Tile? currentTile, double initialRotation, bool rotate) {
+            if (!rotate) {
+                if (currentTile != null) {
+                    image.Source = ((ListViewEntry)listview_tiles.Items[Array.FindIndex(map.Tiles, tile => tile.Name == currentTile?.Name)]).Image;
+                } else {
+                    image.Source = defaultImage;
+                }
+                ((RotateTransform)image.RenderTransform).Angle = initialRotation;
+            } else {
+                ((RotateTransform)image.RenderTransform).Angle = (((RotateTransform)image.RenderTransform).Angle + 90) % 360;
+            }
+        }
+
+        private void CommandBinding_Next_Executed (ref int index, ref TileBrushStrokeCollection brushRef, Image image, BitmapImage defaultImage, float initialRotation) {
+            index++;
+            if (index > brushRef.Count) index = 0;
+            ImageClick(image, defaultImage, (brushRef.Count > index) ? brushRef[index].Tile : (Tile?)null, initialRotation, false);
+        }
+
+        #region Image_MouseDown
         private void Image_CornerTL_MouseDown (object sender, MouseButtonEventArgs e) {
             SetTile(ref DialogResultBrush.CTL, ref indexCTL, image_cornertl);
             ImageClick(image_cornertl, image_d, (DialogResultBrush.CTL.Count > indexCTL) ? DialogResultBrush.CTL[indexCTL].Tile : (Tile?)null, 90, e.RightButton == MouseButtonState.Pressed);
@@ -115,88 +151,115 @@ namespace mapKnight.ToolKit.Windows.Dialogs {
             SetTile(ref DialogResultBrush.LTR, ref indexLTR, image_Ltr);
             ImageClick(image_Ltr, image_L, (DialogResultBrush.LTR.Count > indexLTR) ? DialogResultBrush.LTR[indexLTR].Tile : (Tile?)null, 180, e.RightButton == MouseButtonState.Pressed);
         }
+        #endregion
 
-        private void Button_NextCentre_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexCentre, ref DialogResultBrush.Centre, image_centre, image_O, 0f);
+        #region CommandBindings
+        #region Next* CommandBindings
+        private void CommandBinding_NextCornerTL_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.CTL.Count > 0;
         }
 
-        private void Button_NextCTL_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexCTL, ref DialogResultBrush.CTL, image_cornertl, image_d, 90f);
+        private void CommandBinding_NextCornerTL_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexCTL, ref DialogResultBrush.CTL, image_cornertl, image_d, 90f);
         }
 
-        private void Button_NextCTR_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexCTR, ref DialogResultBrush.CTR, image_cornertr, image_d, 180f);
+        private void CommandBinding_NextCornerTR_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.CTR.Count > 0;
         }
 
-        private void Button_NextCBR_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexCBR, ref DialogResultBrush.CBR, image_cornerbr, image_d, 270f);
+        private void CommandBinding_NextCornerTR_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexCTR, ref DialogResultBrush.CTR, image_cornertr, image_d, 180f);
         }
 
-        private void Button_NextCBL_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexCBL, ref DialogResultBrush.CBL, image_cornerbl, image_d, 0f);
+        private void CommandBinding_NextCornerBL_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.CBL.Count > 0;
         }
 
-        private void Button_NextIL_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexIL, ref DialogResultBrush.IL, image_Il, image_I, 90f);
+        private void CommandBinding_NextCornerBL_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexCBL, ref DialogResultBrush.CBL, image_cornerbl, image_d, 0f);
         }
 
-        private void Button_NextIT_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexIT, ref DialogResultBrush.IT, image_It, image_I, 180f);
+        private void CommandBinding_NextCornerBR_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.CBR.Count > 0;
         }
 
-        private void Button_NextIR_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexIR, ref DialogResultBrush.IR, image_Ir, image_I, 270f);
+        private void CommandBinding_NextCornerBR_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexCBR, ref DialogResultBrush.CBR, image_cornerbr, image_d, 270f);
         }
 
-        private void Button_NextIB_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexIB, ref DialogResultBrush.IB, image_Ib, image_I, 0f);
+        private void CommandBinding_NextCentre_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.Centre.Count > 0;
         }
 
-        private void Button_NextLBL_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexLBL, ref DialogResultBrush.LBL, image_Lbl, image_L, 0f);
+        private void CommandBinding_NextCentre_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexCentre, ref DialogResultBrush.Centre, image_centre, image_O, 0f);
         }
 
-        private void Button_NextLBR_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexLBR, ref DialogResultBrush.LBR, image_Lbr, image_L, 270f);
+        private void CommandBinding_NextIL_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.IL.Count > 0;
         }
 
-        private void Button_NextLTL_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexLTL, ref DialogResultBrush.LTL, image_Ltl, image_L, 90f);
+        private void CommandBinding_NextIL_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexIL, ref DialogResultBrush.IL, image_Il, image_I, 90f);
         }
 
-        private void Button_NextLTR_Click (object sender, RoutedEventArgs e) {
-            ButtonNextClick(ref indexLTR, ref DialogResultBrush.LTR, image_Ltr, image_L, 180f);
+        private void CommandBinding_NextIT_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.IT.Count > 0;
         }
 
-        private void ButtonNextClick (ref int index, ref TileBrushStrokeCollection brushRef, Image image, BitmapImage defaultImage, float initialRotation) {
-            index++;
-            if (index > brushRef.Count) index = 0;
-            ImageClick(image, defaultImage, (brushRef.Count > index) ? brushRef[index].Tile : (Tile?)null, initialRotation, false);
+        private void CommandBinding_NextIT_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexIT, ref DialogResultBrush.IT, image_It, image_I, 180f);
         }
 
-        private void SetTile (ref TileBrushStrokeCollection brushRef, ref int index, Image image) {
-            if (listview_tiles.SelectedItem != null) {
-                if (brushRef.Count <= index) {
-                    brushRef.Add(new TileBrushStroke(map.Tiles[listview_tiles.SelectedIndex], (float)(((RotateTransform)image.RenderTransform).Angle / 180), 1));
-                } else {
-                    brushRef[index] = new TileBrushStroke(map.Tiles[listview_tiles.SelectedIndex], (float)(((RotateTransform)image.RenderTransform).Angle / 180), 1);
-                }
-            }
+        private void CommandBinding_NextIR_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.IR.Count > 0;
         }
 
-        private void ImageClick (Image image, BitmapImage defaultImage, Tile? currentTile, double initialRotation, bool rotate) {
-            if (!rotate) {
-                if (currentTile != null) {
-                    image.Source = ((ListViewEntry)listview_tiles.Items[Array.FindIndex(map.Tiles, tile => tile.Name == currentTile?.Name)]).Image;
-                } else {
-                    image.Source = defaultImage;
-                }
-                ((RotateTransform)image.RenderTransform).Angle = initialRotation;
-            } else {
-                ((RotateTransform)image.RenderTransform).Angle = (((RotateTransform)image.RenderTransform).Angle + 90) % 360;
-            }
+        private void CommandBinding_NextIR_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexIR, ref DialogResultBrush.IR, image_Ir, image_I, 270f);
         }
 
+        private void CommandBinding_NextIB_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.IB.Count > 0;
+        }
+
+        private void CommandBinding_NextIB_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexIB, ref DialogResultBrush.IB, image_Ib, image_I, 0f);
+        }
+
+        private void CommandBinding_NextLTL_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.LTL.Count > 0;
+        }
+
+        private void CommandBinding_NextLTL_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexLTL, ref DialogResultBrush.LTL, image_Ltl, image_L, 90f);
+        }
+
+        private void CommandBinding_NextLTR_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.LTR.Count > 0;
+        }
+
+        private void CommandBinding_NextLTR_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexLTR, ref DialogResultBrush.LTR, image_Ltr, image_L, 180f);
+        }
+
+        private void CommandBinding_NextLBL_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.LBL.Count > 0;
+        }
+
+        private void CommandBinding_NextLBL_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexLBL, ref DialogResultBrush.LBL, image_Lbl, image_L, 0f);
+        }
+
+        private void CommandBinding_NextLBR_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = DialogResultBrush.LBR.Count > 0;
+        }
+
+        private void CommandBinding_NextLBR_Executed (object sender, ExecutedRoutedEventArgs e) {
+            CommandBinding_Next_Executed(ref indexLBR, ref DialogResultBrush.LBR, image_Lbr, image_L, 270f);
+        }
+        #endregion
+        
         private void CommandBinding_Finish_CanExecute (object sender, CanExecuteRoutedEventArgs e) {
             e.CanExecute = DialogResultBrush.IsValid(map);
         }
@@ -214,6 +277,15 @@ namespace mapKnight.ToolKit.Windows.Dialogs {
         private void CommandBinding_EditPossibilities_Executed (object sender, ExecutedRoutedEventArgs e) {
             DialogResultBrush.GeneratePreviewImages(map);
             new EditBrushPossibilitiesDialog(DialogResultBrush).ShowDialog( );
+        }
+        #endregion
+
+        private struct ListViewEntry {
+            public ListViewEntry (BitmapImage image) {
+                Image = image;
+            }
+
+            public BitmapImage Image { get; private set; }
         }
     }
 }
